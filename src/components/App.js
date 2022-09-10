7770217d3b50047910a00b32f2fc806e1c194348
ciri-ryan <cirijaye@yahoo.com>
2022-09-10 07:25:08 -0700

    setup pages and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
       <div className="App">
-        App
+        <Routes>
+            <Route path='/'  element={ <StreamList /> } />
+            <Route path='/streams/new' element={ <StreamCreate /> } />
+            <Route path='/streams/edit'  element={ <StreamEdit /> } />
+            <Route path='/streams/delete' element={ <StreamDelete /> } />
+            <Route path='/streams/show' element={ <StreamShow /> } />
+        </Routes>
       </div>
     );
   }
---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const StreamCreate = () => {
+  return (
+    <div>
+        StreamCreate
+        <Link to='/'>Click Home</Link>
+    </div>
+  )
+}
+
+export default StreamCreate;
