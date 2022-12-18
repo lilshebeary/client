4a387503a23710e5f03432ff48c12e04219a35d8
ciri-ryan <cirijaye@yahoo.com>
2022-12-18 05:40:41 -0800

    add packages, Provider, redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers } from "@reduxjs/toolkit";
+import reducers from '../reducers';
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -7,19 +11,23 @@ import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 
+const store = configureStore(reducers);
+
 const App = () => {
-    return (
+  return (
+    <Provider store={store}>
       <div className="ui container">
         <Header />
         <Routes>
-            <Route path='/'  element={ <StreamList /> } />
-            <Route path='/streams/new' element={ <StreamCreate /> } />
-            <Route path='/streams/edit'  element={ <StreamEdit /> } />
-            <Route path='/streams/delete' element={ <StreamDelete /> } />
-            <Route path='/streams/show' element={ <StreamShow /> } />
+          <Route path="/" element={<StreamList />} />
+          <Route path="/streams/new" element={<StreamCreate />} />
+          <Route path="/streams/edit" element={<StreamEdit />} />
+          <Route path="/streams/delete" element={<StreamDelete />} />
+          <Route path="/streams/show" element={<StreamShow />} />
         </Routes>
       </div>
-    );
-  }
-  
-  export default App;
+    </Provider>
+  );
+};
+
+export default App;
